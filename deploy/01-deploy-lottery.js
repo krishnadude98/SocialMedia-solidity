f2ec2f9120495e7a88e7e6fddbbb28df303c1147
hariaccubits <hari.krishna@accubits.com>
2023-02-28 14:04:06 +0530

    lottery contract done

---
+++ deploy/01-deploy-lottery.js
@@ -0,0 +1,53 @@
+const { network, ethers } = require("hardhat");
+const { developmentChains, networkConfig } = require("../helper-hardhat-config");
+const VRF_SCRIPTION_AMOUNT = ethers.utils.parseEther("2");
+const { verify } = require("../utils/verify");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+    let vrfCoordinatorV2Address, subsriptionId;
+
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+        const transactionReciept = await transactionResponse.wait(1);
+        subsriptionId = transactionReciept.events[0].args.subId;
+        await vrfCoordinatorV2Mock.fundSubscription(subsriptionId, VRF_SCRIPTION_AMOUNT);
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
+        subsriptionId = networkConfig[chainId]["subsriptionId"];
+    }
+    const entranceFee = networkConfig[chainId]["entranceFee"];
+    const gasLane = networkConfig[chainId]["gasLane"];
+    const callbackGasLimit = networkConfig[chainId]["callBackGasLimit"];
+    const interval = networkConfig[chainId]["interval"];
+    const args = [
+        vrfCoordinatorV2Address,
+        entranceFee,
+        gasLane,
+        subsriptionId,
+        callbackGasLimit,
+        interval,
+    ];
+    const lottery = await deploy("Lottery", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    });
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY &&
+        process.env.POLYGON_API_KEY
+    ) {
+        console.log("VERIFYING.........");
+        await verify(lottery.address, args);
+    }
+
+    console.log("------------------------------------");
+};
+module.exports.tags = ["all", "lottery"];
---
+++ hardhat.config.js
@@ -0,0 +1,58 @@
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+const GORELLI_RPC_URL = process.env.GORELLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const API_KEY = process.env.ETHERSCAN_API_KEY;
+const POLYGON_API_KEY = process.env.POLYGON_API_KEY;
+const COIN_MARKET_CAP_API_KEY = process.env.COIN_MARKET_CAP_API_KEY;
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        gorelli: {
+            url: GORELLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        polygon_mumbai: {
+            url: "https://polygon-mumbai.g.alchemy.com/v2/kTZxIUR6MKogwu-aarjjs5euIVls4j0j",
+            accounts: [PRIVATE_KEY],
+            blockConfirmations: 6,
+            chainId: 80001,
+        },
+    },
+    solidity: "0.8.8",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            goerli: API_KEY,
+            polygonMumbai: POLYGON_API_KEY,
+        },
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COIN_MARKET_CAP_API_KEY,
+        token: "MATIC",
+    },
+};
